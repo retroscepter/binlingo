c7653557a93c08ce2c42412fceef5d77ee6e1dcf
Braden Lamb <octavetoast@gmail.com>
2021-03-26 00:28:03 -0400

    Include one-run samples in benchmarks

--- tests/benchmark.js
+++ tests/benchmark.js
@@ -2,30 +2,35 @@ const Benchmark = require('benchmark')
 const { Writer, DynWriter } = require('../lib')
 
 const INT8_COUNT = 3000
+const WRITER_LABEL = 'Writer#finalize (setup and finalize)'
+const DYNWRITER_LABEL = 'DynWriter#finalize (setup and finalize)'
 
-const suite = new Benchmark.Suite()
+console.time(WRITER_LABEL)
 
 const writer = new Writer(0, INT8_COUNT)
-const dynWriter = new DynWriter(0)
 
 for (let i = 0; i < INT8_COUNT; i++) {
     const int = Math.floor(Math.random() * 256)
     writer.writeInt8(int)
+}
+
+writer.finalize()
+
+console.timeEnd(WRITER_LABEL)
+console.time(DYNWRITER_LABEL)
+
+const dynWriter = new DynWriter(0)
+
+for (let i = 0; i < INT8_COUNT; i++) {
+    const int = Math.floor(Math.random() * 256)
     dynWriter.writeInt8(int)
 }
 
-const writerLabel = 'Writer#finalize (1 run sample)'
-const dynWriterLabel = 'DynWriter#finalize (1 run sample)'
-
-console.time(writerLabel)
-writer.finalize()
-console.timeEnd(writerLabel)
-
-console.time(dynWriterLabel)
 dynWriter.finalize()
-console.timeEnd(dynWriterLabel)
 
-suite
+console.timeEnd(DYNWRITER_LABEL)
+
+new Benchmark.Suite()
     .add('Writer#finalize', function () {
         writer.finalize()
     })
